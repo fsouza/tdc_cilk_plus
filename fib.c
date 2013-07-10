40e0bf30a674dc30596dd9ca6e25a488c7baf405
Francisco Souza <f@souza.cc>
2013-07-10 12:43:59 -0300

    fib: adicionando arquivo

---
+++ fib.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <cilk/cilk.h>
+
+int
+fib(int n)
+{
+	if(n < 2) {
+		return n;
+	}
+	int x, y;
+	x = cilk_spawn fib(n-1);
+	y = fib(n-2);
+	cilk_sync;
+	return x + y;
+}
+
+int
+main(void)
+{
+	printf("%d\n", fib(30));
+	return 0;
+}
