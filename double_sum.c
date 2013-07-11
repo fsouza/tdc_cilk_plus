49630d1ddf220e853125f6885a4006b7c14fdebc
Francisco Souza <f@souza.cc>
2013-07-11 00:02:12 -0300

    all: banners de copyright

--- double_sum.c
+++ double_sum.c
@@ -1,3 +1,7 @@
+// Copyright 2013 Francisco Souza. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #include <stdio.h>
 #include <cilk/cilk.h>
 #include <cilk/reducer_opadd.h>
