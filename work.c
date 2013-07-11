9da8d462dcda97474217efaea75a0064992d3243
Francisco Souza <f@souza.cc>
2013-07-10 23:23:28 -0300

    work: exemplo sem cilk_for

---
+++ work.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cilk/cilk.h>
+
+#define N 10
+
+void
+do_something(int i)
+{
+	printf("Doing something with %d\n", i);
+}
+
+int
+main(void)
+{
+	int i;
+	for(i = 0; i < N; i++) {
+		cilk_spawn do_something(i);
+	}
+	cilk_sync;
+	return 0;
+}
