5e1c831458ef80f3c1aba42070e99175f6074a16
Francisco Souza <f@souza.cc>
2013-07-11 01:14:54 -0300

    saxpy: fazendo uma implementação básica

--- saxpy.f90
+++ saxpy.f90
@@ -2,11 +2,13 @@
 ! Use of this source code is governed by a BSD-style
 ! license that can be found in the LICENSE file.
 
-program sum_arrays
+program saxpy
    implicit none
-   integer, dimension(10) :: a, b, c
-   a = 300
-   b = 20
-   c = a + b
-   print *, c
+   real(kind=4), dimension(10) :: x, y
+   real(kind=4) :: a
+   x = 300
+   y = 20
+   a = 10
+   y = y + a*x
+   print *, y
 end program
