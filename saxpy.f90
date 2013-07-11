b4456651dece91db04f7c084d88b607ac10a470e
Francisco Souza <f@souza.cc>
2013-07-11 15:16:08 -0300

    saxpy: usando subroutine para ficar mais parecido com exemplo em C

--- saxpy.f90
+++ saxpy.f90
@@ -2,13 +2,21 @@
 ! Use of this source code is governed by a BSD-style
 ! license that can be found in the LICENSE file.
 
-program saxpy
+subroutine saxpy(x, y, a, n)
+   integer, intent(in) :: n
+   real(kind=4), dimension(n), intent(in) :: x
+   real(kind=4), dimension(n), intent(inout) :: y
+   real(kind=4), intent(in) :: a
+   y = y + a*x
+end subroutine
+
+program saxpyp
    implicit none
    real(kind=4), dimension(10) :: x, y
    real(kind=4) :: a
    x = 300
    y = 20
    a = 10
-   y = y + a*x
+   call saxpy(x, y, a, 10)
    print *, y
 end program
