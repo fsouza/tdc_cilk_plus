35ecb735cc790b37719fde0154900e1773e65f2a
Francisco Souza <f@souza.cc>
2013-07-12 02:10:38 -0300

    multiply_intrinsic: corrigindo código
    Esse código só é compatível com vetores múltiplos de 4.

--- multiply_intrinsic.c
+++ multiply_intrinsic.c
@@ -3,6 +3,7 @@
 // license that can be found in the LICENSE file.
 
 #include <stdio.h>
+#include <string.h>
 #include <smmintrin.h>
 #include "dump.h"
 
@@ -22,10 +23,10 @@ multiply(int *a, int *b, int *c, int n)
 int
 main(void)
 {
-	int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	int b[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 10};
-	int c[10];
-	multiply(a, b, c, 10);
-	dump(c, 10, "%d ");
+	int a[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+	int b[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+	int c[8];
+	multiply(a, b, c, 8);
+	dump(c, 8, "%d ");
 	return 0;
 }
